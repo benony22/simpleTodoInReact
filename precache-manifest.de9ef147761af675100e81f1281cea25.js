31578b66eb05e17fd160a73dcdc9eced3564d4b0
benony22 <binibtc@gmail.com>
2020-11-20 22:05:40 +0300

    Updates

---
+++ precache-manifest.de9ef147761af675100e81f1281cea25.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c78dd3ff83c3006601e1937339a0ad9b",
+    "url": "/simpleTodoInReact/index.html"
+  },
+  {
+    "revision": "369b5838d36c55158235",
+    "url": "/simpleTodoInReact/static/css/main.180865df.chunk.css"
+  },
+  {
+    "revision": "2a4336bbc5ce52062c6e",
+    "url": "/simpleTodoInReact/static/js/2.93bf7d46.chunk.js"
+  },
+  {
+    "revision": "65075b20798fa24012a1ad453c78c90d",
+    "url": "/simpleTodoInReact/static/js/2.93bf7d46.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "369b5838d36c55158235",
+    "url": "/simpleTodoInReact/static/js/main.dfe1f562.chunk.js"
+  },
+  {
+    "revision": "5fd66678146c1c935a0f",
+    "url": "/simpleTodoInReact/static/js/runtime-main.12f99ead.js"
+  }
+]);
